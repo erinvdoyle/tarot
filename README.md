14da98f9a0c28b1536b4409b579076b9ca9fa508
Erin Doyle <erin.v.doyle@gmail.com>
2024-08-27 20:41:37 +0000

    add javascript toggle feature to the 'Get Your Reading' button and 'Test Your Knowledge' button so that each displays its corresponding div. Updated README and index with tutorial credit for this feature and added the necessary css for functionality

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Main Image: https://www.freepik.com/free-vector/tarot-concept-illustration_19710
 
 ### Tutorials
 Glowing Text Tutorial from w3 Schools, "How to Create A Glowing Text": https://www.w3schools.com/howto/howto_css_glowing_text.asp
+
+JavaScript Toggle Tutorial from W3 Schools, "How TO - Toggle Hide and Show"
+https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
---
+++ assets/script.js
@@ -0,0 +1,28 @@
+/* Tutorial Credit: W3 Schools: How TO - Toggle Hide and Show */
+
+/**
+ * Toggle button displays the Tarot Reading div on click
+ */
+
+function displayReading() {
+    var x = document.getElementById("tarot-reading");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  /**
+ * Toggle button displays the Tarot Reading div on click
+ */
+
+  function displayQuiz() {
+    var x = document.getElementById("tarot-quiz");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
