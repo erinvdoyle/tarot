2e5f24990caa30d5be0b9590f9d7fcbd00ee4025
Erin Doyle <erin.v.doyle@gmail.com>
2024-08-28 18:58:30 +0000

    add styling to quiz container and answer button, create array for quiz questions in script.js

--- assets/script.js
+++ assets/script.js
@@ -25,4 +25,18 @@ function displayReading() {
       x.style.display = "none";
     }
   }
+
+  /* Array of quiz questions and answers */
+
+  const questions = [
+    {
+        question: "What is the number of the Fool card in the Major Arcana?",
+        options: ["0", "1", "2", "3"],
+        answer: 0
+    },
+    {
+        question: "Which Major Arcana card represents new beginnings and adventures?",
+        options: ["The Magician", "The Fool", "The High Priestess", "The Empress"],
+        answer: 1
+    }];
   
